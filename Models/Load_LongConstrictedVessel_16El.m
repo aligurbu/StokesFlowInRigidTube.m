cbd8b267d90b6c7bcced4e8696388545df11ecab
aligurbu <aligurbu@buffalo.edu>
2021-11-19 01:35:28 -0800

    Input the model and parameters for the analysis

---
+++ Models/Load_LongConstrictedVessel_16El.m
@@ -0,0 +1,21 @@
+%% Load mesh
+%% Vessel models
+load LongConstrictedVessel_16El.mat
+name = nameVessel;
+
+%% Plotting the geometry of the model
+if verbose_Plot
+    figure('Color','white')
+    Plot_Mesh(coord, connect, 0.25, true, false)
+    axis off
+end
+if verbose_Patch
+    figure('Color','white')
+    Patch_Mesh(coord, connect)
+end
+
+%% Reference parameters
+ReferenceParameters
+
+%% Parameters 
+ParametersForTheAnalysis

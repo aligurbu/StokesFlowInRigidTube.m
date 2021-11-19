35b69af5536bc9186132d9c84f10500296b6680e
aligurbu <aligurbu@buffalo.edu>
2021-11-19 02:25:37 -0800

    Update ParametersForTheAnalysis.m
    - Set the nodal boundary conditions
    - Set element boundary conditions
    - Set prescribed (inlet) traction
    - Set prescribed velocity (all zero for now)
    - Array to store element DOF numbers

--- Models/ParametersForTheAnalysis.m
+++ Models/ParametersForTheAnalysis.m
@@ -36,3 +36,43 @@ numNodesPerElem = size(connect,1); % Number of nodes per element
 
 numDofPerElem = numNodesPerElem * numDofPerNode; 
                                  % Number of DOF associated with an element
+
+%% Set the nodal boundary conditions
+% bcs == 1; Dirichlet B.C; velocity is known
+% bcs == 0; Neumann B.C; traction is known
+bcs = zeros(size(coord));
+bcs(:) = 1;
+bcs(:,setdiff(inletnode,wallnode)) = 0;
+bcs(:,setdiff(outletnode,wallnode)) = 0;
+
+DirichletNode = wallnode;
+NeumannNode = union(setdiff(inletnode,wallnode), ...
+                    setdiff(outletnode,wallnode));
+
+DirichletDofs = find(bcs == 1); % Dofs of the wall nodes
+NeumannDofs = find(bcs == 0);   % Dofs of the inlet/outlet nodes 
+                                % excluding dofs related to the edge nodes.
+
+%% Set element boundary conditions
+bcselem = zeros(1, size(connect,2));
+bcselem(1,wallelem) = 1; % Dirichlet B.C; velocity is known
+
+DirichletElem = wallelem; % = find(bcselem);
+NeumannElem = union(inletelem,outletelem); % = find(~bcselem);
+
+%% Set prescribed (inlet) traction
+Telem = zeros(numDofPerElem, numElem);
+%% Prescribe the inlet pressure on the inlet element nodes in x-direction
+Telem(1:numDofPerNode:numDofPerElem,inletelem) = InletPressure; 
+
+%% Set prescribed velocity (all zero for now)
+unodal = zeros(numDofPerNode, numNodes);
+
+%% Array to store element DOF numbers
+elemDofNum = zeros(numDofPerElem, numElem);
+for m = 1:numElem
+    for n = 1:numNodesPerElem
+        elemDofNum((n-1)*numDofPerNode+(1:numDofPerNode),m) = ...
+                          (connect(n,m)-1)*numDofPerNode+(1:numDofPerNode);
+    end
+end

4b0178858efadc89f5861efb6cb2cae9ab2de1c3
aligurbu <aligurbu@buffalo.edu>
2021-11-19 19:49:55 -0800

    Create PlotSettings.m

---
+++ Visualize/PlotSettings.m
@@ -0,0 +1,8 @@
+%% Settings 
+LineWidthind = 1.1; % Line width
+MarkerSizeind = 20; % Marker size
+Scaleind = 1; % Scaling index for quiver3
+MaxHeadSizeind = 0.7;
+TransparencyInd = 0.8;
+FaceAlphaind = 0.5;
+set(0,'defaulttextinterpreter','latex')

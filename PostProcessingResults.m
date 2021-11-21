23b7ef533ba702c482d7cd56cc269705f3ace70b
aligurbu <aligurbu@buffalo.edu>
2021-11-20 16:49:07 -0800

    Update PostProcessingResults.m

--- PostProcessingResults.m
+++ PostProcessingResults.m
@@ -27,6 +27,7 @@ VelocityInsideDomain = VelocityInsideDomain*RefVelocity*10^(-3); %mm/sec
 PlotSettings
 
 %% Contour graph of speed of the fluid inside the vessel
+%% Compute the speed of the fluid flow
 [LineSpeed, CrossSectionSpeedmid, CrossSectionSpeedinlet, ...
             CrossSectionSpeedoutlet, MinCaxis, MaxCaxis] = ...
             SpeedComputations(LinePts, LinePtsX, ...
@@ -35,3 +36,12 @@ PlotSettings
                               CirclePtsoutlet, CirclePtsXoutlet, ...
                               VelocityInsideDomain);
 
+%% Contour graph 
+ContourSpeedInsideVessel
+
+%% Maximum velocity
+InletPressure = InletPressure*RefPressure; % Pa = N/m^2
+TubeRadius = TubeRadius*RefLength; % micron
+TubeLength = TubeLength*RefLength; % micron
+mu = mu*RefViscosity; % Pa.s = N.s/m^2
+Vmax = ((InletPressure * TubeRadius^2) / (4*mu*TubeLength))*10^(-3) %mm/s
